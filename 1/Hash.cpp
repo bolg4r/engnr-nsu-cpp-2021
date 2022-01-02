34316ecfedcead96c0b1f737067d441f11e2be5e
Rostislav <poc105108@gmail.com>
2022-01-03 01:18:13 +0700

    wyd

--- 1/Hash.cpp
+++ 1/Hash.cpp
@@ -9,9 +9,9 @@
 uint32_t adler32( std::istream &file ){
     uint32_t s1 = 1;
     uint32_t s2 = 0;
-    unsigned char ch;
-    while( file.read((char *)(&ch), sizeof(unsigned char)) ){
-        s1 = ( s1 + (ch) ) % 65521;
+    unsigned char sig;
+    while( file.read((char *)(&sig), sizeof(unsigned char)) ){
+        s1 = ( s1 + sig ) % 65521;
         s2 = ( s2 + s1 ) % 65521;
     }
     return ( s2 << 16 ) | s1;
@@ -20,14 +20,14 @@ uint32_t adler32( std::istream &file ){
 uint64_t sum64( std::istream &file ){
     uint64_t sum = 0, point = 0, result = 0;
     unsigned char sig;
-    while (file.read((char*) &sig, sizeof(unsigned char))){
+    while (file.read((char*)&sig, sizeof(unsigned char))){
         if(point == 8){
-            sum += result;
+            sum = sum + result;
             result = 0;
             point = 0;
         }
         result = result << 8;
-        result += sig;
+        result = result + sig;
         point++;
     }
 
