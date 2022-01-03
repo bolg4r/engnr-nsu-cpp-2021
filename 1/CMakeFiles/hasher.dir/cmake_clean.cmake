a4384c23ecbeaf04dcfe39829a1886dc14bca697
Rostislav <poc105108@gmail.com>
2022-01-03 14:36:14 +0700

    hihihi

---
+++ 1/CMakeFiles/hasher.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hasher.dir/Hash.cpp.obj"
+  "CMakeFiles/hasher.dir/Hash.cpp.obj.d"
+  "CMakeFiles/hasher.dir/main.cpp.obj"
+  "CMakeFiles/hasher.dir/main.cpp.obj.d"
+  "hasher.exe"
+  "hasher.exe.manifest"
+  "hasher.pdb"
+  "libhasher.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hasher.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
