3dca94b80bb8c596339df66c46af140fb20a8d68
Rostislav <poc105108@gmail.com>
2022-01-09 12:16:26 +0700

    d trash

--- 2/for/libs/googletest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/study/pluss/engnr-nsu-cpp-2021/2/libs/googletest
-# Build directory: C:/study/pluss/engnr-nsu-cpp-2021/2/for/libs/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
